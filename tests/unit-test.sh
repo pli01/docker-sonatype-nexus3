cbdda4f22b29c7e638a545b9e65fcf195a5b0b93
pli01 <pli01@github.com>
2018-02-20 16:32:28 +0000

    prefix compose test namespace

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -3,20 +3,21 @@ set -x
 
 image_name=${1:? $(basename $0) IMAGE_NAME VERSION needed}
 version=${2:-latest}
+namespace=nexus
 
 ret=0
 echo "Check tests/docker-compose.yml config"
-docker-compose config
+docker-compose -p ${namespace} config
 test_result=$?
 if [ "$test_result" -eq 0 ] ; then
-  echo "[PASSED] docker-compose config"
+  echo "[PASSED] docker-compose -p ${namespace} config"
 else
-  echo "[FAILED] docker-compose config"
+  echo "[FAILED] docker-compose -p ${namespace} config"
   ret=1
 fi
 
 echo "Check nexus installed"
-docker-compose run --name "test-nexus" --rm nexus ls -l /opt/sonatype/nexus/
+docker-compose -p ${namespace} run --name "test-nexus" --rm nexus ls -l /opt/sonatype/nexus/
 test_result=$?
 if [ "$test_result" -eq 0 ] ; then
   echo "[PASSED] nexus installed"
@@ -33,22 +34,21 @@ echo "# setup env test:"
 test_compose=docker-compose.yml
 test_service=nexus
 test_config=nexus-test.sh
-docker-compose -f $test_compose up -d --no-build $test_service
-docker-compose -f $test_compose ps $test_service
-container=$(docker-compose  -f $test_compose ps -q $test_service)
+docker-compose -p ${namespace} -f $test_compose up -d --no-build $test_service
+docker-compose -p ${namespace} -f $test_compose ps $test_service
+container=$(docker-compose -p ${namespace}  -f $test_compose ps -q $test_service)
 echo docker cp $test_config ${container}:/opt
 docker cp $test_config ${container}:/opt
 
 # run test
 echo "# run test:"
-docker-compose  -f $test_compose exec -T $test_service /bin/bash -c "/opt/$test_config"
+docker-compose -p ${namespace}  -f $test_compose exec -T $test_service /bin/bash -c "/opt/$test_config"
 test_result=$?
 
 # teardown
 echo "# teardown:"
-docker-compose  -f $test_compose stop
-docker-compose  -f $test_compose rm -fv
-docker system prune -f
+docker-compose -p ${namespace}  -f $test_compose stop
+docker-compose -p ${namespace}  -f $test_compose rm -fv
 
 if [ "$test_result" -eq 0 ] ; then
   echo "[PASSED] nexus url check [$test_config]"
